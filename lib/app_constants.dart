8ae37cbe1becfe2c4e6de1c75c1b974239500d65
SatendraPal <palsatyendra9984@gmail.com>
2022-10-08 20:00:01 +0530

    commit

---
+++ lib/app_constants.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+TextStyle kHintTextStyle = TextStyle(
+  fontSize: 20,
+  color: Colors.grey.shade500,
+  fontWeight: FontWeight.w500,
+);
+BoxDecoration kContainerElevationDecoration = BoxDecoration(
+  border: Border.all(
+    color: Colors.grey.shade400,
+  ),
+  color: Colors.white,
+  borderRadius: BorderRadius.circular(10),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.grey.shade400,
+      offset: const Offset(0.5, 6.5), //(x,y)
+      blurRadius: 4.5,
+    ),
+    BoxShadow(
+      color: Colors.grey.shade400,
+      offset: const Offset(-0.5, -0.5), //(x,y)
+      blurRadius: 4.5,
+    ),
+  ],
+);
